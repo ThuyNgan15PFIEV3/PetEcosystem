c7f1db6bba8c6ced28a3829f3ae99b580fef25a6
Thiên Trương <tdthien1997@gmail.com>
2019-05-15 22:11:10 +0700

    Add remote mongodb url in database.config

--- Back-end/server/config/database.config.js
+++ Back-end/server/config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/PetEcosystem'
+  url: 'mongodb://localhost:27017/PetEcosystem',
+  remoteUrl: 'mongodb://booklender321:VVpNcG3DkAbcKAkHoccUHuql8eL1aPxVSj5hM5GshQh8XTr6T2dQRDC6Owbp5eSTC6EIX9t0FD9GWaISy3stuw==@booklender321.documents.azure.com:10255/PetEcosystem?ssl=true&replicaSet=globaldb'
 }
